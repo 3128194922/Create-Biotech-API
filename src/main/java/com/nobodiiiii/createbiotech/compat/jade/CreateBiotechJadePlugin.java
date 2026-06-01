bd696bc22f53eea550d43dba66b0c8bb8a43ee5e
Nobodiiiii <mmu503690@gmail.com>
2026-06-01 13:02:47 +0800

    ui

--- src/main/java/com/nobodiiiii/createbiotech/compat/jade/CreateBiotechJadePlugin.java
+++ src/main/java/com/nobodiiiii/createbiotech/compat/jade/CreateBiotechJadePlugin.java
@@ -7,27 +7,35 @@ import com.nobodiiiii.createbiotech.content.experience.ExperienceCrystallizerBlo
 import com.nobodiiiii.createbiotech.content.experience.ExperienceCrystallizerBlockEntity;
 import com.nobodiiiii.createbiotech.content.experience.ExperienceTankBlock;
 import com.nobodiiiii.createbiotech.content.experience.ExperienceTankBlockEntity;
+import com.nobodiiiii.createbiotech.content.slimemimic.SlimeMimicHandler;
 import com.nobodiiiii.createbiotech.registry.CBItems;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
 
 import snownee.jade.api.BlockAccessor;
+import snownee.jade.api.EntityAccessor;
 import snownee.jade.api.IBlockComponentProvider;
+import snownee.jade.api.IEntityComponentProvider;
 import snownee.jade.api.IServerDataProvider;
 import snownee.jade.api.ITooltip;
 import snownee.jade.api.IWailaClientRegistration;
 import snownee.jade.api.IWailaCommonRegistration;
 import snownee.jade.api.IWailaPlugin;
+import snownee.jade.api.Identifiers;
 import snownee.jade.api.WailaPlugin;
 import snownee.jade.api.config.IPluginConfig;
+import snownee.jade.api.theme.IThemeHelper;
 
 @WailaPlugin
 public class CreateBiotechJadePlugin implements IWailaPlugin {
@@ -35,7 +43,10 @@ public class CreateBiotechJadePlugin implements IWailaPlugin {
 	private static final String CURRENT_XP = "CurrentXp";
 	private static final String MAX_XP = "MaxXp";
 
+	private static final String BIONIC_NAME_PREFIX_KEY = "create_biotech.jade.bionic_prefix";
+
 	public static final ResourceLocation EXPERIENCE_INFO = CreateBiotech.asResource("experience_info");
+	public static final ResourceLocation BIONIC_NAME = CreateBiotech.asResource("bionic_name");
 
 	@Override
 	public void register(IWailaCommonRegistration registration) {
@@ -51,6 +62,7 @@ public class CreateBiotechJadePlugin implements IWailaPlugin {
 		registration.registerBlockComponent(ExperienceComponent.INSTANCE, ExperienceTankBlock.class);
 		registration.registerBlockComponent(ExperienceComponent.INSTANCE, ExperienceCrystallizerBlock.class);
 		registration.registerBlockComponent(ExperienceComponent.INSTANCE, EvokerEnchantingChamberBlock.class);
+		registration.registerEntityComponent(BionicNameComponent.INSTANCE, LivingEntity.class);
 	}
 
 	public enum ExperienceComponent implements IBlockComponentProvider {
@@ -78,6 +90,34 @@ public class CreateBiotechJadePlugin implements IWailaPlugin {
 		@Override
 		public ResourceLocation getUid() {
 			return EXPERIENCE_INFO;
+		}
+	}
+
+	/**
+	 * Prefixes the name of bionic creatures (slime mimics) with the "Bionic" label.
+	 * Runs after Jade's core {@code object_name} provider (priority -10100, so the name is
+	 * already present), then replaces that line with a prefixed copy.
+	 */
+	public enum BionicNameComponent implements IEntityComponentProvider {
+		INSTANCE;
+
+		@Override
+		public void appendTooltip(ITooltip tooltip, EntityAccessor accessor, IPluginConfig config) {
+			Entity entity = accessor.getEntity();
+			if (!SlimeMimicHandler.isSlimeMimic(entity))
+				return;
+			if (tooltip.get(Identifiers.CORE_OBJECT_NAME).isEmpty())
+				return;
+
+			MutableComponent prefixedName = Component.translatable(BIONIC_NAME_PREFIX_KEY)
+				.append(entity.getDisplayName());
+			tooltip.remove(Identifiers.CORE_OBJECT_NAME);
+			tooltip.add(0, IThemeHelper.get().title(prefixedName), Identifiers.CORE_OBJECT_NAME);
+		}
+
+		@Override
+		public ResourceLocation getUid() {
+			return BIONIC_NAME;
 		}
 	}
 
